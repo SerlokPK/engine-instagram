e4e890e633555760efc9f66b1d106f10b3ee3a45
serbulas1 <serbulas1@gmail.com>
2020-08-17 19:07:16 +0200

    Configuration for EF enabled and first migration executed

---
+++ Repository/Migrations/202008171659489_UserModelAdded.cs
@@ -0,0 +1,29 @@
+﻿namespace Repository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UserModelAdded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserId = c.Int(nullable: false),
+                        Email = c.String(maxLength: 255),
+                        Username = c.String(maxLength: 50),
+                        Password = c.String(maxLength: 50),
+                        Description = c.String(maxLength: 2000),
+                    })
+                .PrimaryKey(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
