1776e23973f1997d51ded37dd67c65b791c92f64
serbulas1 <serbulas1@gmail.com>
2020-08-21 19:26:51 +0200

    Added regex validation for whitespace

--- Models/ModelAttributes/Regex.cs
+++ Models/ModelAttributes/Regex.cs
@@ -3,5 +3,6 @@
     public class Regex
     {
         public const string Email = @"\A(?:[a-zA-Z0-9!#$%&'*+/=?^_{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)\Z";
+        public const string NoWthispace = @"^\S*$";
     }
 }
