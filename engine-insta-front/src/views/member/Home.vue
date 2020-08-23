86917afba05fe31932e430bcb6a85fb5fbfd528c
serbulas1 <serbulas1@gmail.com>
2020-08-23 20:12:44 +0200

    removed logout from home

--- engine-insta-front/src/views/member/Home.vue
+++ engine-insta-front/src/views/member/Home.vue
@@ -1,24 +1,12 @@
 <template>
   <div>
     Ovo je smao probno
-    <b-button
-      variant="danger"
-      @click="logOut"
-    >
-      Log out
-    </b-button>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
-
 export default {
-  methods: {
-    ...mapActions({
-      logOut: 'logOut'
-    })
-  }
+
 };
 </script>
 
