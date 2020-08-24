4c2a40acf0ab7df23df7f4eccdb388af6c621e84
serbulas1 <serbulas1@gmail.com>
2020-08-24 18:51:49 +0200

    Preview mode for account finished

---
+++ engine-insta-front/src/components/ui/Account/PreviewMode.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="d-flex flex-column align-items-center">
+    <b-form-group
+      :label="$t('account.emailLabel')"
+      label-for="email"
+      class="w-50"
+    >
+      <b-form-input
+        id="email"
+        v-model="user.email"
+        disabled
+        :placeholder="$t('account.emailPlaceholder')"
+      />
+    </b-form-group>
+
+    <b-form-group
+      :label="$t('account.usernameLabel')"
+      label-for="username"
+      class="w-50"
+    >
+      <b-form-input
+        id="username"
+        v-model="user.username"
+        disabled
+        :placeholder="$t('account.usernamePlaceholder')"
+      />
+    </b-form-group>
+
+    <div class="d-flex justify-content-between w-50 mt-3">
+      <b-button
+        variant="info"
+      >
+        {{ $t('account.editButton') }}
+      </b-button>
+      <b-button
+        variant="danger"
+      >
+        {{ $t('account.deleteButton') }}
+      </b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        user: {
+            type: Object,
+            default: () => {}
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
--- engine-insta-front/src/views/member/Account.vue
+++ engine-insta-front/src/views/member/Account.vue
@@ -1,10 +1,39 @@
 <template>
-  <div>Account page</div>
+  <div>
+    <preview-mode :user="user" />
+    <!-- <b-tabs
+      class="block"
+      nav-class="nav-tabs-block"
+      content-class="block-content overflow-hidden"
+    >
+      <b-tab
+        class="fade-left"
+        title="Change username"
+      >
+        <p>Testcina1</p><p>Testcin1a</p><p>Testci1na</p><p>Test1cina</p><p>Testcina</p>
+      </b-tab>
+      <b-tab
+        class="fade-left"
+        title="Title dobnar 2"
+      >
+        <p>Testcina</p><p>Testcina</p><p>Testcina</p><p>Testcina</p><p>Testcina</p>
+      </b-tab>
+    </b-tabs> -->
+  </div>
 </template>
 
 <script>
-export default {
+import PreviewMode from '../../components/ui/Account/PreviewMode';
 
+export default {
+  components: {
+    PreviewMode
+  },
+  data() {
+    return {
+      user: {}
+    };
+  }
 };
 </script>
 
